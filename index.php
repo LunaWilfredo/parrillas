eabb415ec87d462a710b490a60d51de65a9002e0
Wilfredo Luna <61670708+LunaWilfredo@users.noreply.github.com>
2021-07-01 21:49:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 <?php 
 
-if(isset($_GET['pagina'])){
-    $pagina = $_GET['pagina'];
-    if($pagina =="parrillas"){
-        require_once "view/content/".$pagina.".php";
-    }else{
-        require_once 'view/content/parrillas.php';
-    }
-}else{
+// if(isset($_GET['pagina'])){
+//     $pagina = $_GET['pagina'];
+//     if($pagina =="parrillas"){
+//         require_once "view/content/".$pagina.".php";
+//     }else{
+//         require_once 'view/content/parrillas.php';
+//     }
+// }else{
     require_once 'view/content/parrillas.php';
-}
+// }
 
 ?>
 
