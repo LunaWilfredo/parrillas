05c92471540d85e285297789b1b496593763fe41
Wilfredo Luna <61670708+LunaWilfredo@users.noreply.github.com>
2021-07-01 21:54:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
-<?php require_once "view/layout/header.php"?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parrillas</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/styles.css">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+  <!-- cabecera -->
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Parrillas</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!-- Fin de cabecera -->
+  <div class="container">
 
 <!-- Contenido -->
 <h1 class="display-0">Para Fanáticos de las Parrillas</h1>
@@ -156,5 +184,14 @@
 
     </div>
 
+    </div>
+  <footer class="text-center">
+    Design by Wluna
+  </footer>        
 
-<?php require_once 'view/layout/footer.php'?>
+  <!-- JS BOOTSTRAP -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  
+</body>
+</html>
